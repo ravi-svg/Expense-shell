0df0662c5a43f79ed82d128930505fcbdbe5e7e0
vadlakondaravi <ravivadlakonda10@gmail.com>
2024-09-03 06:47:18 +0530

    expense

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 log_folder="/var/lag/shell-script"
-echo "This script's name is: $0"
-echo Argument 1: $1
-echo Argument 2: $2
+
+echo "script name"
+echo $0
+
+name=$(echo "mysql.sh" | cut -d "." -f1)
+
+echo "scriptname:"
+$script-name
