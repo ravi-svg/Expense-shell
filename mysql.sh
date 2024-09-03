b91ea452129602a52a38481bc521de5994526b8a
vadlakondaravi <ravivadlakonda10@gmail.com>
2024-09-03 06:29:54 +0530

    expense

--- mysql.sh
+++ mysql.sh
@@ -5,11 +5,7 @@ log_folder="/var/lag/shell-script"
 echo "script name"
 echo $0
 
-spit=$(echo "ravi.sh" | cut -d "." -f1)
-echo "split"
-$split
-
-script-name=$(echo $0 | cut -d "." -f1)
+script-name=$(echo "mysql.sh" | cut -d "." -f1)
 
 echo "scriptname:"
 $script-name
